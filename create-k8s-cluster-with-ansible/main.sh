be181777454fde282d819880e6e05521e78bd84b
Kamalesh Kumar Giri <83784951+nerdlaunda@users.noreply.github.com>
2021-09-17 17:50:18 +0530

    Update main.sh

--- create-k8s-cluster-with-ansible/main.sh
+++ create-k8s-cluster-with-ansible/main.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # set dry_run to 1 to enable check, and 0 to execute the playbooks
 dry_run=1
-
+key_path=/home/user/ansible-kubernetes/create-k8s-cluster-with-ansible/1-user-setup/
 
 opr=""
 if [ $dry_run -eq 1 ] 
@@ -12,6 +12,6 @@ if [ $dry_run -eq 1 ]
 fi
 
 # ansible begin
-ansible-playbook -i ./1-user-setup/inventory ./1-user-setup/main.yaml -e "master_ssh_key_path=/home/user/ansible-kubernetes/create-k8s-cluster-with-ansible/1-user-setup/master-key worker_ssh_key_path=/home/user/ansible-kubernetes/create-k8s-cluster-with-ansible/1-user-setup/worker-key" -K $opr
+ansible-playbook -i ./1-user-setup/inventory ./1-user-setup/main.yaml -e "master_ssh_key_path=$key_path/master-key worker_ssh_key_path=$key_path/worker-key" -K $opr
 
 ansible-playbook -i inventory-final ./2-k8s-prerequisite/main.yaml $opr
