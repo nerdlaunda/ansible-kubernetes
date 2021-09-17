be5fa689c6ae960d19e4e2556b50e52d92a028ff
nerdlaunda <nerdlaunda@gmail.com>
2021-09-17 17:20:26 +0530

    user mod complete

---
+++ create-k8s-cluster-with-ansible/main.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+# set dry_run to 1 to enable check, and 0 to execute the playbooks
+dry_run=1
+
+
+opr=""
+if [ $dry_run -eq 1 ] 
+	then
+		opr="--check"
+	else
+		opr=""
+fi
+
+# ansible begin
+ansible-playbook -i ./1-user-setup/inventory ./1-user-setup/main.yaml -e "master_ssh_key_path=/home/user/ansible-kubernetes/create-k8s-cluster-with-ansible/1-user-setup/master-key worker_ssh_key_path=/home/user/ansible-kubernetes/create-k8s-cluster-with-ansible/1-user-setup/worker-key" -K $opr
+
+ansible-playbook -i inventory-final ./2-k8s-prerequisite/main.yaml $opr
